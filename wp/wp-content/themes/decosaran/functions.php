0266e007df973a75eaaa61a55e3c450d4a9d4c57
Milad Abooali <m.abooali@hotmail.com>
2020-06-02 05:57:39 +0430

    WP Enqueues css, js and img folder

--- wp/wp-content/themes/decosaran/functions.php
+++ wp/wp-content/themes/decosaran/functions.php
@@ -1,3 +1,19 @@
 <?php
 
-define('F_IMG',get_template_directory_uri().'/assets/img');
+
+/*
+* Enqueues
+*/
+function enqueue_assets(){
+# Enqueues Scripts
+  foreach( glob( get_template_directory(). '/assets/js/*.js' ) as $file ) {
+    wp_enqueue_script( $file, get_template_directory_uri().'/assets/js/'.$file);
+  }
+# Enqueues Styles
+  foreach( glob( get_template_directory(). '/assets/css/*.css' ) as $file ) {
+    wp_enqueue_style( $file, get_template_directory_uri().'/assets/css/'.$file);
+  }
+}
+add_action('wp_enqueue_scripts', 'enqueue_assets');
+# Enqueues IMG Folder
+define('F_IMG', get_template_directory().'/assets/img');
